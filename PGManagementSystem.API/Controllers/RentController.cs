237bae1311e68b9bbfb8f7287f943dc734c971dc
krunal <kunalmistry0177@gmail.com>
2026-08-11 00:00:51 +0530

    commit 100826

--- PGManagementSystem.API/Controllers/RentController.cs
+++ PGManagementSystem.API/Controllers/RentController.cs
@@ -45,7 +45,7 @@ namespace PGManagementSystem.Controllers
         /// PG Owner: Record Payment from a Tenant
         /// </summary>
         [HttpPost("record-payment")]
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "Tenant")]
         public async Task<IActionResult> RecordPayment([FromBody] RecordPaymentDto dto)
         {
             try
